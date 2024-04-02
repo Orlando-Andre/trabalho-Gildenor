f7eb201b7580cb3aa9c5c4a799ff3679c56b164d
Orlando André Souza Neto <orlandoandre019@gmail.com>
2024-04-02 20:38:46 -0300

    Update popUpRastrear.js

--- front-end/paginas/popUpRastrear.js
+++ front-end/paginas/popUpRastrear.js
@@ -1,5 +1,17 @@
 function fechar() {
 
-    
+    /*Endereço da tela que será referenciada*/
+    const url = 'home.html';
+
+    escondeDados();
+
+    // Obtendo a referência ao elemento iframe
+    const iframe = document.querySelector('.iframe-Content');
+    iframe.classList.toggle('ativo')
+
+    //Defina a URL do iframe para carregar o conteúdo do arquivo HTML
+    iframe.src = url;
+
+
     
 }
